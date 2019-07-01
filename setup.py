02db017820049e58ad7c0ce2dcdce030e95f8b80
Arturo Curiel <curieldaz@gmail.com>
2019-07-01 11:21:52 -0500

    Creating egg

--- setup.py
+++ setup.py
@@ -1,28 +1,31 @@
 from distutils.core import setup
 
 setup(
-    name = 'multilang-summarization',         # How you named your package folder (MyLib)
-    packages = ['multilang_summarization'],   # Chose the same as "name"
-    version = '1',      # Start with a small number and increase it with every change you make
-    license='GPLv3',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-    author = 'Arturo Curiel',                   # Type in your name
-    author_email = 'me@arturocuriel.com',      # Type in your E-Mail
-    url = 'arturocuriel.com',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-    keywords = ['SUMMARIZATION', 'MULTILANGUAGE', 'KEYWORDS'],   # Keywords that define your package best
-    install_requires=[            # I get to this in a second
-            'validators',
-            'beautifulsoup4',
+    name = 'multilang-summarization',
+    packages = ['multilang_summarization'],
+    version = '1',
+    license='GPLv3',
+    description = 'Multilanguage summarizer, intended to improve text readability',
+    author = 'Arturo Curiel',
+    author_email = 'me@arturocuriel.com',
+    url = 'arturocuriel.com',
+    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+    keywords = ['SUMMARIZATION', 'MULTILANGUAGE', 'RULE-BASED'],
+    install_requires=[
+            'nltk',
+            'pyphen',
+            'textstat',
+            'sentence-splitter',
         ],
     classifiers=[
-      'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-      'Intended Audience :: Developers',      # Define that your audience are developers
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
       'Topic :: Software Development :: Build Tools',
-      'License :: OSI Approved :: MIT License',   # Again, pick a license
-      'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+      'OSI Approved :: GNU General Public License v3 (GPLv3)'
+      'Programming Language :: Python :: 3',
       'Programming Language :: Python :: 3.4',
       'Programming Language :: Python :: 3.5',
       'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',
     ],
 )
